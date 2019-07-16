074e9ecf68d5ee8202f9bc366a505395632dc6c6
Zakusilo.DV <zakusilo.dv@gutagroup.ru>
2019-07-16 16:35:13 +0300

    Add debug info/

--- src/lib/index.js
+++ src/lib/index.js
@@ -11,15 +11,37 @@ const $sBaseClass = 'visualcr';
  */
 export class VisualCR {
     init($obConfig) {
+
+        /** @const HTMLElement $obDebug Subject for debug info. */
+        let $obDebug = document.createElement('div');
+        $obDebug.classList.add($sBaseClass + '_button');
+        $obDebug.innerHTML = 'VCR Debug info.';
+
+        let $obDebugTooltip = document.createElement('div');
+        $obDebugTooltip.classList.add($sBaseClass + '_tooltip');
+
+
         for (let $sLink in $obConfig) {
 
             /** @const HTMLElement $obParser Subject of the tag of the link. */
             const $obParser = document.createElement('a');
+
             $obParser.href = $sLink;
 
             /** @internal If the current page is found in a config, then we receive elements which need to be allocated.  */
             if (window.location.pathname === $obParser.pathname || $sLink === '*') {
+
+                /**  @internal Add info to debug tooltip.  */
+                let $obDebugTooltipContent = document.createElement('div');
+
+                let $obDebugLink =  document.createElement('a');
+                $obDebugLink.href = $sLink;
+                $obDebugLink.innerHTML = $sLink;
+
+                $obDebugTooltipContent.appendChild($obDebugLink);
+
                 for (let $sAnchor in $obConfig[$sLink]) {
+
                     let $arElements = document.querySelectorAll($sAnchor);
                     /** @internal We add a class and tooltip to each element.  */
                     $arElements.forEach(function ($obElement) {
@@ -36,11 +58,30 @@ export class VisualCR {
                         tippy($obElement, {
                             content: '[' + $sClassName + '] ' +$obConfig[$sLink][$sAnchor][0],
                             interactive: true,
-                        })
+                        });
 
+                        // Add content
+                        let $obContent = document.createElement('div');
+                        $obContent.classList.add($sBaseClass + '_content');
+                        $obContent.classList.add($sBaseClass + '_content__' + $sClassName);
+                        $obContent.innerHTML = $obConfig[$sLink][$sAnchor][0];
+                        $obDebugTooltipContent.appendChild($obContent);
                     });
+
                 }
+
+                // Add debug info.
+                $obDebugTooltip.appendChild($obDebugTooltipContent);
+
             }
+
+            tippy($obDebug, {
+                content: $obDebugTooltip,
+                interactive: true,
+                theme: 'light',
+                trigger: 'click'
+            });
+            document.body.appendChild($obDebug);
         }
     }
 };
