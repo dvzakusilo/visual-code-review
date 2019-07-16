a88ac690bf3e35bb5716557f53502b09e5613d7e
Zakusilo.DV <zakusilo.dv@gutagroup.ru>
2019-07-16 17:48:55 +0300

    Debug for all pages / remove duplicate in debug list.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 'use strict';
-import tippy from 'tippy.js'
+import tippy from 'tippy.js';
 
 const $sBaseClass = 'visualcr';
 
@@ -20,61 +20,60 @@ export class VisualCR {
         let $obDebugTooltip = document.createElement('div');
         $obDebugTooltip.classList.add($sBaseClass + '_tooltip');
 
+        /**  @internal Add info to debug tooltip.  */
+        let $obDebugTooltipContent = document.createElement('div');
 
         for (let $sLink in $obConfig) {
 
             /** @const HTMLElement $obParser Subject of the tag of the link. */
             const $obParser = document.createElement('a');
-
             $obParser.href = $sLink;
 
+            /** @const HTMLElement $obDebugLink Links in tooltip. */
+            let $obDebugLink = document.createElement('a');
+            $obDebugLink.href = ($sLink !== '*') ? $sLink : '#';
+            $obDebugLink.innerHTML = $sLink;
 
+            $obDebugTooltipContent.appendChild($obDebugLink);
 
-                /**  @internal Add info to debug tooltip.  */
-                let $obDebugTooltipContent = document.createElement('div');
+            for (let $sAnchor in $obConfig[$sLink]) {
 
-                let $obDebugLink =  document.createElement('a');
-                $obDebugLink.href = $sLink;
-                $obDebugLink.innerHTML = $sLink;
+                /** @internal If class not allowed, then add class "error". */
+                const $sClassName = (typeof ($obConfig[$sLink][$sAnchor][1]) === 'undefined') ? 'error' : $obConfig[$sLink][$sAnchor][1];
 
-                $obDebugTooltipContent.appendChild($obDebugLink);
+                // Add content
+                let $obContent = document.createElement('div');
+                $obContent.classList.add($sBaseClass + '_content');
+                $obContent.classList.add($sBaseClass + '_content__' + $sClassName);
+                $obContent.innerHTML = $obConfig[$sLink][$sAnchor][0];
+                $obDebugTooltipContent.appendChild($obContent);
 
-                for (let $sAnchor in $obConfig[$sLink]) {
-
-                    let $arElements = document.querySelectorAll($sAnchor);
+                /** @internal If the current page is found in a config, then we receive elements which need to be allocated.  */
+                if (window.location.pathname === $obParser.pathname || $sLink === '*') {
                     /** @internal We add a class and tooltip to each element.  */
+                    let $arElements = document.querySelectorAll($sAnchor);
+
                     $arElements.forEach(function ($obElement) {
 
-                        $obElement.setAttribute("data-tooltip", $obConfig[$sLink][$sAnchor][0]);
+                        $obElement.setAttribute('data-tooltip', $obConfig[$sLink][$sAnchor][0]);
 
                         /** @internal If class not allowed, then add class "error". */
-                        const $sClassName = (typeof ($obConfig[$sLink][$sAnchor][1]) === 'undefined') ? 'error' : $obConfig[$sLink][$sAnchor][1];
+
                         $obElement.classList.add($sBaseClass);
                         $obElement.classList.add($sBaseClass + '__' + $sClassName);
 
-                        /** @internal If the current page is found in a config, then we receive elements which need to be allocated.  */
-                        if (window.location.pathname === $obParser.pathname || $sLink === '*') {
-                            // Add popup
+                        // Add popup
 
-                            tippy($obElement, {
-                                content: '[' + $sClassName + '] ' + $obConfig[$sLink][$sAnchor][0],
-                                interactive: true,
-                            });
-                        }
-
-                        // Add content
-                        let $obContent = document.createElement('div');
-                        $obContent.classList.add($sBaseClass + '_content');
-                        $obContent.classList.add($sBaseClass + '_content__' + $sClassName);
-                        $obContent.innerHTML = $obConfig[$sLink][$sAnchor][0];
-                        $obDebugTooltipContent.appendChild($obContent);
+                        tippy($obElement, {
+                            content: '[' + $sClassName + '] ' + $obConfig[$sLink][$sAnchor][0],
+                            interactive: true,
+                        });
                     });
-
                 }
+            }
 
-                // Add debug info.
-                $obDebugTooltip.appendChild($obDebugTooltipContent);
-
+            // Add debug info.
+            $obDebugTooltip.appendChild($obDebugTooltipContent);
 
 
             tippy($obDebug, {
